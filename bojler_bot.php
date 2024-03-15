fe161afb305606e1ee32672aef48740ec7f443f5
Márton Polgár <37218286+2colours@users.noreply.github.com>
2024-03-15 03:43:11 +0100

    fix: syntax error at method chaining

--- bojler_bot.php
+++ bojler_bot.php
@@ -240,7 +240,7 @@ $bot = new DiscordCommandClient([
     'prefix' => 'b!',
     'token' => $_ENV['DC_TOKEN'],
     'description' => 'Szórakodtató bot',
-    'logger' => new Logger('devnull')->pushHandler(new NullHandler()),
+    'logger' => (new Logger('devnull'))->pushHandler(new NullHandler()),
     'discordOptions' => [
         'intents' => Intents::getDefaultIntents()
         //      | Intents::MESSAGE_CONTENT, // Note: MESSAGE_CONTENT is privileged, see https://dis.gd/mcfaq
