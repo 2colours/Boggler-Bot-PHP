d560437fdc5c47681cb8fe164bd0a0ffbde27c48
2colours <2colours@github.com>
2024-03-15 13:09:22 +0100

    fix: introduce >0 requirement for most hints solved

--- bojler_game_status.php
+++ bojler_game_status.php
@@ -601,7 +601,9 @@ class GameStatus
         $solved_hints = fn ($player_data) => count(array_intersect($player_data['found_words'], $player_data['used_hints']));
         $awards['Newcomer'] = array_filter($relevant_players, fn ($player) => $is_newcomer($this->player_handler->player_dict[$player]));
         $most_solved_hints_amount = max(array_map(fn ($player) => $solved_hints($this->player_handler->player_dict[$player]), $relevant_players));
-        $awards['Most solved hints'] = array_filter($relevant_players, fn ($player) => $solved_hints($this->player_handler->player_dict[$player]) === $most_solved_hints_amount);
+        if ($most_solved_hints_amount > 0) {
+            $awards['Most solved hints'] = array_filter($relevant_players, fn ($player) => $solved_hints($this->player_handler->player_dict[$player]) === $most_solved_hints_amount);
+        }
         return $awards;
     }
 }
